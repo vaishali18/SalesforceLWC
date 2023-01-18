af89de47243de0fe2b8ed969bc91b2442faa5e87
vaishalig <vaishalig@VAISHALI-LAP>
2023-01-18 18:31:50 +0530

    Chartjs in LWC

---
+++ force-app/main/default/lwc/chartCard/chartCard.js
@@ -0,0 +1,65 @@
+import { LightningElement } from 'lwc';
+import chartjs from '@salesforce/resourceUrl/chartjs';
+import { loadScript } from 'lightning/platformResourceLoader';
+
+export default class ChartCard extends LightningElement {
+    error;
+    chart;
+    chartjsInitialized = false;
+
+    config = {
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    data: [
+                        10,
+                        15,
+                        20,
+                        12,
+                        14
+                    ],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)'
+                    ],
+                    label: 'Dataset 1'
+                }
+            ],
+            labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue']
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    position: 'right'
+                }
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    };
+
+    renderedCallback() {
+        if (this.chartjsInitialized) {
+            return;
+        }
+        this.chartjsInitialized = true;
+
+        loadScript(this, chartjs)
+            .then(() => {
+                const canvas = document.createElement('canvas');
+                this.template.querySelector('div.chart').appendChild(canvas);
+                const ctx = canvas.getContext('2d');
+                this.chart = new window.Chart(ctx, this.config);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+    }
+}
